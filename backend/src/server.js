eeaac2aea4848045721ab693bd6e60bb4a57a169
manishrawat000 <manishrawatji000@gmail.com>
2026-03-29 21:21:22 +0530

    error detect in file dirc

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,10 +10,10 @@
  app.use("/api/auth", authrouter);
  app.use("/api/message", messageRouter);
  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
     app.use((req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
     });
 }
  app.listen(PORT, () => console.log('Server running on port :' + PORT));
