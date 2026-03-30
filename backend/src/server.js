0dbe37d9199cbb2ead8606c3d284d317bd079c06
manishrawat000 <manishrawatji000@gmail.com>
2026-03-30 18:40:01 +0530

    created whole project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,35 @@
- import express from 'express';
- import dotenv from 'dotenv';
- import authrouter from './routes/auth.router.js';
- import messageRouter from './routes/message.router.js';
- import path from 'path';
-import {connectDB} from './lib/db.js';
-import { connect } from 'http2';
- dotenv.config();
- const PORT = process.env.PORT || 3000;
- const __dirname = path.resolve();
- const app = express();
- app.use(express.json());// to parse json data from request body
- app.use("/api/auth", authrouter);
- app.use("/api/message", messageRouter);
- if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
 
-    app.use((req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
-    });
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
+import { app, server } from "./lib/socket.js";
+
+const __dirname = path.resolve();
+
+const PORT = ENV.PORT || 3000;
+
+app.use(express.json({ limit: "5mb" })); // req.body
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
- app.listen(PORT, () => {
-    console.log("Server is running on port:" + PORT);
-    connectDB();
-});
 
+server.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+  connectDB();
+});
